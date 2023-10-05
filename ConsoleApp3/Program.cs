6e43fbe7e91ec6e6a9af5cb2716f898ef52bce3a
NamNguyen261101 <nguyenkhanhnam2001.12@gmail.com>
2023-10-05 19:30:02 +0700

    Add project Lab2.4

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,42 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("\na)");
+        int i, j;
+        for (i = 1; i <= 6; i++)
+        {
+            Console.WriteLine();
+            for (j = 1; j <= i; j++)
+            {
+                Console.Write("*");
+            }         
+        }
+        Console.WriteLine("\nb)");
+        for (i = 6; i >= 1; i--)
+        {
+            Console.WriteLine();
+            for(j = 1; j <= i; j++)
+            {
+                Console.Write("$");
+            }
+        }
+        Console.WriteLine("\nc)");
+        for( i = 0; i <=5; i++)
+        {
+            Console.WriteLine();
+            for(j = 1; j <= 11; j++)
+            {
+                if( j >= 6 - i && j <= 6 + i)
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+        }
+    }
+}
